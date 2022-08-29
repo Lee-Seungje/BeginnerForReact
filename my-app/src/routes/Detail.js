7ff6510b7639c626ab285fb6ed6afeab333574e1
Seungjee Lee <moondgod@gmail.com>
2022-08-29 17:56:00 +0900

    all

--- my-app/src/routes/Detail.js
+++ my-app/src/routes/Detail.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+import {useParams} from "react-router-dom";
+
 function Detail() {
-    return <h1>Detail</h1>;
+    const [isLoading, setIsLoading] = useState(true);
+    const [JSON, setJSON] = useState(null);
+    const {id} = useParams();
+    const getMovie = async()=>{
+        const json = await(await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+        setJSON(json.data.movie.title);
+    }
+    useEffect(() => {
+        getMovie();
+        setIsLoading(false);
+    }, [])
+    return <h1>{isLoading ? "Detail" : <h1>{JSON}</h1>}</h1>;
     }
 export default Detail;
